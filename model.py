7a6a4e9952fae33436e22b036cb7cde0d8ed42dd
quincyhopper <216158136+quincyhopper@users.noreply.github.com>
2026-04-01 14:43:19 +0100

    Remove transformer model class

--- model.py
+++ model.py
@@ -14,25 +14,3 @@ class LogisticRegression(nn.Module):
     
     def save(self, path):
         torch.save(self.state_dict(), path)
-    
-class Transformer(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.encoder = AutoModel.from_pretrained('roberta-large')
-
-        for param in self.encoder.parameters():
-            param.requires_grad = False
-
-        self.fc1 = nn.Linear(self.encoder.config.hidden_size, 2)
-
-    def forward(self, input_ids, attention_mask):
-        with torch.no_grad():
-            outputs = self.encoder(input_ids, attention_mask)
-
-        embedding = outputs.last_hidden_state[:, -1, :]
-
-        return self.fc1(embedding)
-    
-    def save(self, path):
-        torch.save(self.fc1.state_dict(), path)
